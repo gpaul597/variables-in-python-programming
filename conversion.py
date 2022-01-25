c90b4d6a431b3b45b846dcdae02cbc3911456d90
gpaul597 <ngugipaul597@gmail.com>
2022-01-26 00:58:04 +0300

    second commit

---
+++ conversion.py
@@ -0,0 +1,12 @@
+x=2.3
+a=5
+c="True"
+z= int(x)#converting float to int
+b=float(a)#converting int to float
+d=bool(c)#converting string to bool
+print(z)
+print(b)
+print(d)
+print(type(z))
+print(type(b))
+print(type(d))
